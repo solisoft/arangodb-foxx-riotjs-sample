16bd71adac15fe36a963d16a8b0062efeae66acb
Olivier BONNAURE <olivier@solisoft.net>
2016-08-17 16:15:23 +0300

    dist

--- foxx/login/APP/scripts/send-mail.js
+++ foxx/login/APP/scripts/send-mail.js
@@ -1,6 +1,5 @@
 'use strict';
-const api_key = 'key-24b8d9df24821ca47fd678e95957b476';
-const domain = 'planning-essais.fr';
+/*
 const request = require('@arangodb/request');
 
 request({
@@ -13,3 +12,4 @@ request({
     text: "Testing some Mailgun awesomness!"
   }
 })
+*/
